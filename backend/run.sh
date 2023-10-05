870bac1ad4a61c63e352f921984e31d7c0fe4394
rxn <coppiemertz@gmail.com>
2023-10-05 20:02:28 +0300

    fix: better running script

--- backend/run.sh
+++ backend/run.sh
@@ -5,10 +5,12 @@
 # get date and constant paths
 DATE=`date +"%Y-%m-%d_%T"`
 OAIPWD="/Users/rxn/dev/work/Jolene/backend/api/llama_fork/examples/server"
-MPATH="~/.cache/lm-studio/models/TheBloke/Airoboros-L2-13B-2.1-GGUF/airoboros-l2-13b-2.1.Q5_K_S.gguf"
+MPATH="/Users/rxn/.cache/lm-studio/models/TheBloke/Airoboros-L2-13B-2.1-GGUF/airoboros-l2-13b-2.1.Q5_K_S.gguf"
 APIPWD="/Users/rxn/dev/work/Jolene/backend/api/"
 TGPWD="/Users/rxn/dev/work/Jolene/backend/tgserver/"
 MAINPWD="/Users/rxn/dev/work/Jolene/backend"
+LOGSPWD="${MAINPWD}/logs"
+echo "Logs located in: ${LOGSPWD}"
 # Kill previous servers
 killall server
 killall main
@@ -18,16 +20,27 @@ echo "Made a new database"
 # Start `llama_fork` servers
 ## Start LLaMa.cpp server
 echo "Starting LLAMA server"
-./api/llama_fork/server -m $MPATH -c 2048 -ngl 10 &> llama_server_${DATE}.log &
+CMD="./api/llama_fork/server -m $MPATH -c 2048 -ngl 10 &| tee $LOGSPWD/llama_server_$DATE.log &"
+echo $CMD
+eval $CMD
+
 
 ## Start OAI-like RestAPI server
 echo "Starting OAI-like server"
-(cd $OAIPWD && echo $PWD && $(./start.sh &> $MAINPWD/oai_server_${DATE}.log &))
+CMD="(cd $OAIPWD && echo $PWD && $(./start.sh &| tee $LOGSPWD/oai_server_$DATE.log &))"
+echo $CMD
+eval $CMD
+
+
 
 # Start API server
 echo "Starting API server"
-(cd $APIPWD && go run main.go &| tee $MAINPWD/api_log_${DATE}.log &)
+CMD="(cd $APIPWD && go run main.go &| tee $LOGSPWD/api_log_$DATE.log &)"
+echo $CMD
+eval $CMD
 
 # Start Telegram server
 echo "Starting Telegram server"
-(cd $TGPWD && go run main.go &| tee $MAINPWD/tgserver_log_${DATE}.log)
+CMD="(cd $TGPWD && go run main.go &| tee $LOGSPWD/tgserver_log_$DATE.log)"
+echo $CMD
+eval $CMD
